88d7e484b9bfb3d25a3d2a50129a868807c5cb5d
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-01-11 22:01:30 -0800

    Veutify Demo

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <span>Vuetify Demo</span>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <v-btn large depressed>
+      <v-icon>email</v-icon>
+    </v-btn>
+    <v-btn small depressed fab>
+      <v-icon>favorite</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "home",
+  components: {}
+};
+</script>
