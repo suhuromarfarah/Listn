4b34bb205e31b64691a47ee22c19df70ade67b6a
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-01-13 09:02:21 -0800

    Added Login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: "/login",
+    name: "login",
+    component: Login
+  },
   {
     path: "/",
     name: "dashboard",
---
+++ src/views/Login.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="login">
+    <v-subheader class="grey--text">Login</v-subheader>
+    <v-container class="my-5">
+      <p>This is the Login Page</p>
+    </v-container>
+  </div>
+</template>
