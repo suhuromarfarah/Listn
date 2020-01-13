f38f222fa4a516809e687c686edc8abed6e6f99e
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-01-12 19:20:58 -0800

    Added Margin

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1 subheading class="grey--text">About</h1>
+    <v-container class="my-5">
+      <p>This is the About Page</p>
+    </v-container>
   </div>
 </template>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="dashboard">
     <h1 subheading class="grey--text">Dashboard</h1>
-    <v-btn large depressed>
-      <v-icon>email</v-icon>
-    </v-btn>
-    <v-btn small depressed fab>
-      <v-icon>favorite</v-icon>
-    </v-btn>
+    <v-container class="my-5">
+      <p>This is the Dashboard Page</p>
+      <v-btn large depressed>
+        <v-icon>email</v-icon>
+      </v-btn>
+      <v-btn small depressed fab>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+    </v-container>
   </div>
 </template>
 
