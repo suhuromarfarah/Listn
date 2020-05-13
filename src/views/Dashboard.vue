f0c7e05d70c561f3a4e6775360ed10a413be2251
Suhur Omar <itssuhuromar@gmail.com>
2020-05-13 01:22:24 -0400

    added dashboard image

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,51 +1,16 @@
 <template>
-  <div class="dashboard">
-    <v-subheader class="grey--text">Dashboard</v-subheader>
-    <v-container class="my-5 grey lighten-4">
-      <p>This is the Dashboard Page</p>
-      <v-row wrap>
-        <v-col :xs="4" :md="3">
-          <v-btn outlined block>Button 1</v-btn>
-        </v-col>
-     <v-col :xs="2" :md="3">
-          <v-btn outlined block>Button 2</v-btn>
-        </v-col>
-        <v-col :xs="4" :md="3">
-          <v-btn outlined block>Button 3</v-btn>
-        </v-col>
-      <v-col :xs="2" :md="3">
-          <v-btn outlined block>Button 4</v-btn>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col :xs="6" :md="5">
-          <v-btn outlined block>Button 1</v-btn>
-        </v-col>
-        <v-col :xs="6" :md="5">
-          <v-btn outlined block>Button 2</v-btn>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-btn depressed block outlined>
-            <v-icon>email</v-icon>
-          </v-btn>
-        </v-col>
-        <v-col>
-          <v-btn md6 small depressed fab outlined>
-            <v-icon>favorite</v-icon>
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+  <v-parallax
+    dark
+    src="https://source.unsplash.com/5gn2soeAc40/1600x900"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col class="text-center" cols="12">
+        <h1 class="display-1 font-weight-thin mb-4">Listn</h1>
+        <h4 class="subheading">Make a playlist for a friend today!</h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
 </template>
-
-<script>
-// @ is an alias to /src
-
-export default {
-  name: "dashboard",
-  components: {}
-};
-</script>
