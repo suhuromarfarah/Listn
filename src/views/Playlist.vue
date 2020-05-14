6b7bf39a43af38e95589b62cf9d60a83e7647f92
Suhur Omar <itssuhuromar@gmail.com>
2020-05-14 01:12:43 -0400

    added music player and song lists

---
+++ src/views/Playlist.vue
@@ -0,0 +1,69 @@
+
+<template>
+  <v-card
+    class="mx-10 ma-10"
+    max-width="300"
+    align="center"
+    tile
+  >
+          <v-list-item>
+            <v-list-item-icon class="mx-10">
+              <v-btn icon>
+                <v-icon>mdi-rewind</v-icon>
+              </v-btn>
+            </v-list-item-icon>
+
+            <v-list-item-icon :class="{ 'mx-0': $vuetify.breakpoint.mdAndUp }">
+              <v-btn icon>
+                <v-icon>mdi-pause</v-icon>
+              </v-btn>
+            </v-list-item-icon>
+
+            <v-list-item-icon
+              class="ml-10"
+              :class="{ 'mr-3': $vuetify.breakpoint.mdAndUp }"
+            >
+              <v-btn icon>
+                <v-icon>mdi-fast-forward</v-icon>
+              </v-btn>
+            </v-list-item-icon>
+          </v-list-item>
+
+    <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title>Two-line item</v-list-item-title>
+        <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+        <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title>Two-line item</v-list-item-title>
+        <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+        <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title>Two-line item</v-list-item-title>
+        <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+        <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title>Two-line item</v-list-item-title>
+        <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+        <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title>Two-line item</v-list-item-title>
+        <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+
+  </v-card>
+</template>
