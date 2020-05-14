24bad290f79bc5a9295d4cff299050360bba7d54
Brent Carey <brentcarey@Brents-MacBook-Air.local>
2020-05-14 13:10:40 -0400

    small change to layout

--- src/views/MakePlaylist.vue
+++ src/views/MakePlaylist.vue
@@ -220,7 +220,7 @@ import YouTube from "simple-youtube-api";
       //2. Import load videos fucntion to get video from youtube
         loadVideos() {
       // console.log("pressed");
-      const youtube = new YouTube("AIzaSyDbn27OnUSxSwmqchM9ayuYj0MPqPu40TA");
+      const youtube = new YouTube("AIzaSyDSltiKOPVIfCL8wKR_UhSAgkI8YkS5jz4");
       youtube
         .searchVideos(this.songName + "lyrics", 4)
         .then(results => {
