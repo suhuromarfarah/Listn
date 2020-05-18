3ab4c32ff08f162717f30c5e4d4dcdd250a74cb2
Suhur Omar <itssuhuromar@gmail.com>
2020-05-17 22:36:24 -0400

    changed video backgroud, added footer, and changed font

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card height="100%">
+    <v-footer
+      absolute
+      class="font-weight-medium"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+      <strong>Copyright © 2020 - Suhur Omar-Farah,  Brent Carey,  Hammad Seehar</strong>
+      </v-col>
+    </v-footer>
+  </v-card>
+</template>
