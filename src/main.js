2849426b2af2b0e34093c1a4a8a935da32cf5b70
Hammad Seehar <hammad96@outlook.com>
2020-05-19 20:33:01 -0400

    additional styling

--- src/main.js
+++ src/main.js
@@ -3,14 +3,17 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 import VueYouTubeEmbed from 'vue-youtube-embed'
-import Carousel3d from 'vue-carousel-3d';
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
+
 Vue.use(VueYouTubeEmbed),{
   global: true
 }
 Vue.config.productionTip = false
-Vue.use(Carousel3d);
 new Vue({
   router,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
